144786000c53390d12bedd16ea7a6fdc54e14347
Rujira Kongsomran <biwberry@outlook.com>
2020-01-07 22:40:30 +0700

    Call RecyclerView, Setting Layout and Set Adapter

--- app/src/main/java/com/rujira/recyclerview/MainActivity.java
+++ app/src/main/java/com/rujira/recyclerview/MainActivity.java
@@ -1,6 +1,8 @@
 package com.rujira.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        // Setting Layout
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Setting Adapter
+        String[] libraries = {"Retrofit2", "OkHttp", "Room", "Glide", "RxJava"};
+        MyRecyclerAdapter adapter = new MyRecyclerAdapter(libraries);
+        recyclerView.setAdapter(adapter);
     }
 }
